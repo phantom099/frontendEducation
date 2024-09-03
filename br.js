528addafb3afbe78a2251a32ced7f752671f7dbe
phantom099 <olegbagryantsev@gmail.com>
2024-09-03 19:56:41 +0300

    new js + array math

---
+++ br.js
@@ -0,0 +1,13 @@
+//alert("hello world");
+//var data = confirm("hello world?");
+//if (data) {
+   // alert("hello world");
+//};
+//prompt("number");
+var person = null;
+if (confirm("sure?")){
+    person = prompt("name");
+    alert("hello " + person);
+} else {
+    alert("?")
+}
--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-console.log("hello world")
+var num_1 = 20.4;
+var num_2 = 10;
+var num = num_1/num_2;
+console.log(num + "hello world");
+var str = "12";
+console.log("Math:"+Math.PI);
+
+
+var number = 3;
+var ishh = true;
+var str ="tn";
+switch(str)
+    {
+        case "tn":
+            console.log("true");
+            break; 
+    }
+var arr =[5, true, 5.6, -100];
+arr [3] ="str";
+console.log(arr.length);
+
+var matrix = [
+    [4,5,6,],["str", 5.6],[true, -100]
+];
+matrix[1][0] = true;
+console.log(matrix);
+for(var i=0;i<12;i++);
+{console.log(i);};
+
+var j=0;
+while (j<=10)
+    {
+        console.log(j);
+        j+=1;
+    }   ;
+    var x=100;
+    do 
+    {
+        console.log(x);
+        x++;
+    } 
+    while (x<50);
+
+var arr1 = [5, 7, 3, 8, 12, "str"];
+for (var a=0; i<arr1.length; a++) {
+    console.log(arr[a]);
+};
